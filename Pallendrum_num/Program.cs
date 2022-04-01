37c600bb45681f0161a3551aa93fd7e22e537d74
Admin <Admin@DESKTOP-Q9C1DBG>
2022-04-01 19:28:24 +0530

    Add project files.

---
+++ Pallendrum_num/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pallendrum_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any Number");
+            int n1 = int.Parse(Console.ReadLine());
+            int rev= 0; int m = n1;
+            while (n1 != 0)
+            {
+                int rem = n1 % 10;
+                rev = rev * 10 + rem;
+                n1 /= 10;
+            }
+                if (rev == m)
+                    Console.WriteLine("This is Pallendrum Number");
+                else
+                {
+                    Console.WriteLine("This is not Pallendrum Number");
+                }
+
+
+
+            
+
+
+        }
+    }
+}
